cc5e836ccf902580a4cf081db26ff264928d3c49
Stanislav Vitebskiy <stanislav.vitebskiy@efi.com>
2016-05-11 16:55:16 -0400

    Finished lesson 1.1.

--- src/Unit-1/DoThis/Program.cs
+++ src/Unit-1/DoThis/Program.cs
@@ -11,18 +11,21 @@ namespace WinTail
         static void Main(string[] args)
         {
             // initialize MyActorSystem
-            // YOU NEED TO FILL IN HERE
+            MyActorSystem = ActorSystem.Create("MyActorSystem");
 
             PrintInstructions();
 
             // time to make your first actors!
-            //YOU NEED TO FILL IN HERE
             // make consoleWriterActor using these props: Props.Create(() => new ConsoleWriterActor())
             // make consoleReaderActor using these props: Props.Create(() => new ConsoleReaderActor(consoleWriterActor))
-
+            // SV: IActoveRef does not necessary represent a specific actor C# object:
+            //     This allows for stateless singletons, load-balanced "farms", etc.
+            var writerActorRef = MyActorSystem.ActorOf(Props.Create(() => new ConsoleWriterActor()));
+            var readerActorRef = MyActorSystem.ActorOf(Props.Create(() => new ConsoleReaderActor(writerActorRef)));
 
             // tell console reader to begin
-            //YOU NEED TO FILL IN HERE
+            // SV: An actor will not do anything until it receives a message.
+            readerActorRef.Tell("start");
 
             // blocks the main thread from exiting until the actor system is shut down
             MyActorSystem.AwaitTermination();
